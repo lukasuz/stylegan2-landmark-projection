double normalization fixed

projector_video.py
@@ -331,7 +331,7 @@ def run_projection(
         # projected_w = projected_w_steps[-1]
         # synth_image = G.synthesis(projected_w.unsqueeze(0), noise_mode='const')
         # print(i,f'{outdir}/proj_{0}.png'.format(i))
-        synth_image = (synth_image + 1) * (255/2)
+        # synth_image = (synth_image + 1) * (255/2)
         synth_image = synth_image.permute(0, 2, 3, 1).clamp(
             0, 255).to(torch.uint8)[0].cpu().numpy()
         PIL.Image.fromarray(synth_image, 'RGB').save(f'{outdir}/proj_{i}.png')